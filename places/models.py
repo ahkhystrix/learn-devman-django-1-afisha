d4350b8268f15860368366bc5e250c186bf79b7f
Alexey Kuznetsov <ahk.hystrix@gmail.com>
2020-06-06 13:53:33 +0300

    Add  Feature model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
-# Create your models here.
-print("hello!")
+
+class Feature(models.Model):
+    title             = models.CharField(max_length=255)
+    imgs              = models.TextField()
+    description_short = models.TextField()
+    description_long  = models.TextField()
+    coordinates_lat   = models.FloatField()
+    coordinates_lng   = models.FloatField()
