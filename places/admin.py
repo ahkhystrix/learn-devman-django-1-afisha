08fdf288fb8a86a4a6108ea411cd22a84f21763b
Alexey Kuznetsov <ahk.hystrix@gmail.com>
2020-06-07 09:32:22 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Feature
+from .models import Feature, Image
 
 
 admin.site.register(Feature)
+admin.site.register(Image)
--- places/models.py
+++ places/models.py
@@ -4,7 +4,27 @@ from django.db import models
 class Feature(models.Model):
     title             = models.CharField(max_length=255)
     imgs              = models.TextField()
+    image1 = models.OneToOneField(
+        "Image",
+        related_name='feature1',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    image2 = models.OneToOneField(
+        "Image",
+        related_name='feature2',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
     description_short = models.TextField()
     description_long  = models.TextField()
     coordinates_lat   = models.FloatField()
     coordinates_lng   = models.FloatField()
+
+
+class Image(models.Model):
+    title   = models.CharField(max_length=255)
+    image   = models.ImageField()
+    feature = models.ForeignKey(Feature, related_name="images", on_delete=models.CASCADE)
