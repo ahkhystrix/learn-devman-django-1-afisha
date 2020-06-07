7a6ca9eda110397c867f8c5e479a2b20b0966dd8
Alexey Kuznetsov <ahk.hystrix@gmail.com>
2020-06-07 23:13:04 +0300

    Refactoring (flake8)

--- places/models.py
+++ places/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Feature(models.Model):
-    placeId           = models.CharField(max_length=255)
-    title             = models.CharField(max_length=255)
-    imgs              = models.TextField()
+    placeId = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    imgs = models.TextField()
     image1 = models.OneToOneField(
         "Image",
         related_name='feature1',
@@ -20,12 +20,16 @@ class Feature(models.Model):
         blank=True,
     )
     description_short = models.TextField()
-    description_long  = models.TextField()
-    coordinates_lat   = models.FloatField()
-    coordinates_lng   = models.FloatField()
+    description_long = models.TextField()
+    coordinates_lat = models.FloatField()
+    coordinates_lng = models.FloatField()
 
 
 class Image(models.Model):
-    title   = models.CharField(max_length=255)
-    image   = models.ImageField()
-    feature = models.ForeignKey(Feature, related_name="images", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField()
+    feature = models.ForeignKey(
+        Feature,
+        related_name="images",
+        on_delete=models.CASCADE
+    )
