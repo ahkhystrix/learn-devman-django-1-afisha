9019dd98fabf7abb7e1dafa045f12dd5a845cf7d
Alexey Kuznetsov <ahk.hystrix@gmail.com>
2020-06-09 19:26:32 +0300

    Add photo preview on admin site

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Feature, Image
 
 
 class ImageAdmin(admin.TabularInline):
     model = Image
-    fields = ("image", "position")
+    fields = ("image", "image_preview", "position")
+    readonly_fields = ["image_preview"]
+
+    def image_preview(self, obj):
+        try:
+            return format_html(
+                '<img src="{}" width={}/>',
+                obj.image.url,
+                200,
+            )
+        except Exception as err:
+            print(err)
 
 
 @admin.register(Feature)
